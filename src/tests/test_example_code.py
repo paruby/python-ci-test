0a784605b75e1984a93cdbd265ec30ed4f9949e9
Paul Rubenstein <notmyrealemail@paulrubenstein.co.uk>
2021-03-30 21:41:16 +0200

    violate flake
    Signed-off-by: Paul Rubenstein <notmyrealemail@paulrubenstein.co.uk>

--- src/tests/test_example_code.py
+++ src/tests/test_example_code.py
@@ -2,4 +2,4 @@ from src.code import example_code
 
 
 def test_append_list():
-    assert example_code.append_list([1, 2, 3], 4) == [1, 2, 3, 4]
+    assert example_code.append_list([1,2,3], 4) == [1,2,3,4]
