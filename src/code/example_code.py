28580853d8d573f0d0a020c0c7b16bc6023d3f51
Paul Rubenstein <notmyrealemail@paulrubenstein.co.uk>
2021-03-31 16:59:43 +0200

    merge sort with test
    Signed-off-by: Paul Rubenstein <notmyrealemail@paulrubenstein.co.uk>

--- src/code/example_code.py
+++ src/code/example_code.py
@@ -4,3 +4,23 @@ from typing import List, Any
 def append_list(input_list: List, elem: Any) -> List:
     input_list.append(elem)
     return input_list
+
+
+def merge_sort(input: List[int]) -> List[int]:
+    if len(input) in [0, 1]:
+        return input
+    else:
+        left = merge_sort(input[0: len(input)//2])
+        right = merge_sort(input[len(input)//2:])
+
+        merged = []
+        pl, pr = 0, 0
+        while pl < len(left) and pr < len(right):
+            if left[pl] < right[pr]:
+                merged.append(left[pl])
+                pl += 1
+            else:
+                merged.append(right[pr])
+                pr += 1
+        merged = merged + left[pl:] + right[pr:]
+        return merged
--- src/tests/test_example_code.py
+++ src/tests/test_example_code.py
@@ -1,10 +1,19 @@
 from src.code import example_code
+
 from copy import deepcopy
 from hypothesis import given
 from hypothesis.strategies import lists, integers
+import random
 
 
 @given(input_list=lists(integers()), elem=integers())
 def test_append_list(input_list, elem):
     _input_list = deepcopy(input_list)
     assert example_code.append_list(input_list, elem) == _input_list + [elem]
+
+
+@given(input=lists(integers()).map(sorted))
+def test_merge_sort(input):
+    _input = deepcopy(input)
+    random.shuffle(input)
+    assert example_code.merge_sort(input=input) == _input
